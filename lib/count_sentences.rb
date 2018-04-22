c522e54eb373e008ea57805a02b1ea47db4216f0
Michael Boroski <mjboroski@gmail.com>
2018-04-22 06:03:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    holder=[]
+    self.split('.').each do |a|
+      holder<<a
+    end
+    self.split('?').each do |a|
+      holder<<a
+    end
+    self.split('!').each do |a|
+      holder<<a
+    end
+    return holder.count%2+holder.count/2
   end
 end
